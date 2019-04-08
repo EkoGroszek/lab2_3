1bf4f36c0d876e958ad53ec673d76f7edd806066
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-04-09 01:57:09 +0200

    testCalculateJackardSimilarityWithBothEmptySequences added

---
+++ src/test/java/SimilarityFinderTest.java
@@ -0,0 +1,21 @@
+import edu.iis.mto.similarity.SequenceSearcherDoubler;
+import edu.iis.mto.similarity.SimilarityFinder;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public class SimilarityFinderTest {
+
+    @Test public void testCalculateJackardSimilarityWithBothEmptySequences(){
+        int[] seq1 = new int[0];
+        int[] seq2 = new int[0];
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherDoubler());
+
+        double testValue = 1.0d;
+        double result = similarityFinder.calculateJackardSimilarity(seq1,seq2);
+
+        Assert.assertThat(result, is(testValue));
+    }
+
+}
