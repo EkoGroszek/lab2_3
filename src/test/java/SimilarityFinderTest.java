8da22c499df993a7f91f8611be17891679877065
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-04-09 02:00:20 +0200

    testCalculateJackardSimilarityWithBothSameSequences added

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -7,13 +7,24 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class SimilarityFinderTest {
 
-    @Test public void testCalculateJackardSimilarityWithBothEmptySequences(){
+    @Test public void testCalculateJackardSimilarityWithBothEmptySequences() {
         int[] seq1 = new int[0];
         int[] seq2 = new int[0];
         SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherDoubler());
 
         double testValue = 1.0d;
-        double result = similarityFinder.calculateJackardSimilarity(seq1,seq2);
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+
+        Assert.assertThat(result, is(testValue));
+    }
+
+    @Test public void testCalculateJackardSimilarityWithBothSameSequences() {
+        int[] seq1 = {32, 213, 313};
+        int[] seq2 = {32, 213, 313};
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherDoubler());
+
+        double testValue = 1.0d;
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
 
         Assert.assertThat(result, is(testValue));
     }
