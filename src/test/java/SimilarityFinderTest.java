6115cee4b4a882f499da0150627e48aee5e7e5ce
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-04-09 02:34:23 +0200

    testCalculateJackardSimilarityWithOneEmptySequence added

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -64,4 +64,15 @@ public class SimilarityFinderTest {
         Assert.assertThat(result, is(testValue));
     }
 
+    @Test public void testCalculateJackardSimilarityWithOneEmptySequence() {
+        int[] seq1 = {1, 2, 3};
+        int[] seq2 = {};
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherDoubler());
+
+        double testValue = 0.0d;
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+
+        Assert.assertThat(result, is(testValue));
+    }
+
 }
